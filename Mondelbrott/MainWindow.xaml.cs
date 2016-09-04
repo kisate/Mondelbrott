c8ab7bbcb9f43201fbde4cfb9d5576b3cb2cf6bb
kisate <dhkas@mail.ru>
2016-09-04 23:26:00 +0300

    Lots of stuff

---
+++ Mondelbrott/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Mondelbrott
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<KeyValuePair<Dot, int>> allDots;
+
+        public class Dot
+        {
+            public double x, y;
+            public Dot(double _x, double _y)
+            {
+                x = _x;
+                y = _y;
+            }
+        }
+        public double p, q;
+
+        public Dot CalculateNext(Dot n)
+        {
+            double x, y;
+            x = n.x * n.x - n.y + p;
+            y = 2 * n.x * n.y + q;
+
+            return new Dot(x,y);
+        }
+
+        public void CheckDot()
+        {
+            int i = 0;
+            Dot c = new Dot(p, q);
+            for (i = 0; i < 1000; i++)
+            {
+                c = CalculateNext(c);
+                if (c.x * c.x + c.y * c.y >= 4)
+                {
+                    if (i <= 500) allDots.Add(new KeyValuePair<Dot, int>(c, 0));
+                    if (500 <= i || i < 600) allDots.Add(new KeyValuePair<Dot, int>(c, 1));
+                    if (600 <= i || i < 700) allDots.Add(new KeyValuePair<Dot, int>(c, 2));
+                    if (700 <= i || i < 800) allDots.Add(new KeyValuePair<Dot, int>(c, 3));
+                    if (800 <= i || i < 900) allDots.Add(new KeyValuePair<Dot, int>(c, 4));
+                    if (900 <= i || i < 1000) allDots.Add(new KeyValuePair<Dot, int>(c, 5));
+                    break;
+                }
+            }
+
+            if (i >= 1000) allDots.Add(new KeyValuePair<Dot, int>(c, 6));
+            ChooseNextCoord();
+        }
+
+        public void ChooseNextCoord()
+        {
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void image1_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            lblCoordinates.Content = string.Format("{0}x{1}", e.NewSize.Width, e.NewSize.Height);
+        }
+
+    }
+}
